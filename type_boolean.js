e5ea269f0e02f870456d31fb6d2e96961de5518f
André Paroli <andreparoli1@gmail.com>
2022-03-01 20:26:52 -0300

    Exemplos da aula 1

---
+++ type_boolean.js
@@ -0,0 +1,11 @@
+const num1 = 5;
+const num2 = 10;
+
+console.log(num1 === num2);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
+
+
---
+++ type_number.js
@@ -0,0 +1,8 @@
+const meuNumero = 3;
+
+const num1 = 1;
+const num2 = 2;
+
+const operacaoMatematica = num1 + num2;
+
+console.log(operacaoMatematica);
